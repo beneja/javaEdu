16b55135789ed9781c44c50f352d75eb96dd4518
AlekseiPanov <panov.a.a60@gmail.com>
2023-02-14 16:29:39 +0300

    hw1 done2 comments

--- edu/src/sem1/homework/simpleNumbers.java
+++ edu/src/sem1/homework/simpleNumbers.java
@@ -9,19 +9,19 @@ import java.util.Scanner;
 public class simpleNumbers {
 
     public static List<Integer> getAllPrimesLessThan(int sieveSize) { //Решето Эрастофена
-        var sieve = new boolean[sieveSize];
-        Arrays.fill(sieve, true);
+        var sieve = new boolean[sieveSize];                           //создаётся массив размером до заданного числа. булевые значения самые экономичные по памяти
+        Arrays.fill(sieve, true);                                 //заполняется полностью true
         sieve[0] = false;
         sieve[1] = false;
 
         for (int i = 2; i < sieve.length; i++) {
             if (sieve[i]) {
                 for (int j = 2; i * j < sieve.length; j++) {
-                    sieve[i * j] = false;
-                }
+                    sieve[i * j] = false;                           //если число можео разложить на множители, то оно нам не нужно
+                }                                                   //сами простые множите при этом не меняются
             }
         }
-        List<Integer> primes = new ArrayList<>();
+        List<Integer> primes = new ArrayList<>();                   //преобразование найденный множителей в числа через их индексы
         for (int i = 2; i < sieve.length; i++) {
             if (sieve[i]) {
                 primes.add(i);
@@ -29,6 +29,7 @@ public class simpleNumbers {
         }
         return primes;
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число: ");
